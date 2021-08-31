80aca9ae048fe6e925085b36d4dcd70977b25fc9
Tim <dtimcockerill2@gmail.com>
2021-08-31 17:05:32 -0400

    created News component learning to use fetch api calls

--- GloboTicket/App.js
+++ GloboTicket/App.js
@@ -8,6 +8,7 @@ import Home from './Home';
 import Tickets from './Tickets';
 import Contact from './Contact';
 import TicketPurchase from './TicketPurchase';
+import News from './News';
 
 const Stack = createStackNavigator();
 
@@ -52,6 +53,15 @@ const App: () => React$Node = () => {
             headerTitleAlign: 'center',
             headerTitleStyle: {fontFamily: 'Ubuntu-Regular'},
             headerTitle: 'Purchase Tickets'
+            }}
+          />
+          <Stack.Screen
+          name='News'
+          component={News}
+          options={{
+            headerTitleAlign: 'center',
+            headerTitleStyle: {fontFamily: 'Ubuntu-Regular'},
+            headerTitle: 'Latest News'
             }}
           />
 
